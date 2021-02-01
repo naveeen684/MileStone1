5925b20e493e35f60f1b05233bd1ad99439f7c34
naveenrajapandian <naveenrajapandian@gmail.com>
2021-02-01 21:11:29 +0530

    initial

--- app/src/main/java/com/example/sdui1/network/Network.kt
+++ app/src/main/java/com/example/sdui1/network/Network.kt
@@ -6,7 +6,7 @@ import retrofit2.http.GET
 
 
 interface ApiRequests {
-    @GET("/")
+    @GET("/1")
     suspend fun getPageModel(): HomePage
 
 }
--- app/src/main/java/com/example/sdui1/network/RetrofitClient.kt
+++ app/src/main/java/com/example/sdui1/network/RetrofitClient.kt
@@ -7,8 +7,8 @@ import java.util.concurrent.TimeUnit
 
 
 object RetrofitClient {
-//    private const val BASE_URL = "http://127.0.0.1:8000/"
-    private const val BASE_URL = "https://subset.free.beeceptor.com/"
+    private const val BASE_URL = "http://10.0.2.2:8000/"
+//    private const val BASE_URL = "https://subset.free.beeceptor.com/"
 
 
     private val client: OkHttpClient = OkHttpClient.Builder()
